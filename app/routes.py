58fb2bc3ffaf55c6b933aafca0b78c02e2f2a85e
iantseng1 <tsengian1@gmail.com>
2024-05-26 17:07:16 -0400

    signup and login frontend

--- app/routes.py
+++ app/routes.py
@@ -3,5 +3,11 @@ from flask import Blueprint, render_template
 main = Blueprint('main', __name__)
 
 @main.route('/')
-def index():
+def homepage():
     return render_template('homepage.html')
+@main.route('/signup')
+def signup():
+    return render_template('signup.html')
+@main.route('/login')
+def login():
+    return render_template('login.html')
