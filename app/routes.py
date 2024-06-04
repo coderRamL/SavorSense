21a2b6c50f0f0dd82e69579dfa13770d1a4250d9
iantseng1 <tsengian1@gmail.com>
2024-06-03 21:44:53 -0400

    update

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 from sqlalchemy import create_engine, Column, Integer, String, Table, MetaData
 from sqlalchemy.exc import SQLAlchemyError
 from datetime import datetime
@@ -36,10 +36,48 @@ def homepage():
         return render_template('homepage.html', res = restaurants, restaurant=restaurant_data, len = length, i = i)
     except SQLAlchemyError as e:
         print(f"An error occurred: {e}")
-
-@main.route('/signup')
+@main.route('/signup', methods=['GET', 'POST'])
 def signup():
     return render_template('signup.html')
+    
+@main.route('/success', methods=['GET', 'POST'])
+def success():
+    try:
+        engine = create_engine("postgresql://SavorSense_owner:iX2EMY6TzLlf@ep-shrill-cloud-a40et0x7.us-east-1.aws.neon.tech/SavorSense?sslmode=require")
+        metadata = MetaData()
+        metadata.reflect(bind=engine)
+        users_table = metadata.tables['users']
+
+        if request.method == 'POST':
+            # Get form data from the frontend
+            name = request.form.get('name')
+            username = request.form.get('username')
+            email = request.form.get('email')
+            password = request.form.get('password')
+
+            with engine.connect() as connection:
+                # Check if the user already exists in the database
+                existing_user = connection.execute(users_table.select().where(users_table.c.username == username).where(users_table.c.email == email)).fetchone()
+
+                if existing_user:
+                    # User already exists, handle accordingly (e.g., show an error message)
+                    return render_template('signup.html', error_message="User already exists")
+
+                # Insert the new user into the database
+                connection.execute(users_table.insert().values(name=name, username=username, email=email, password=password))
+                connection.commit()
+
+                # Redirect to a success page or login page
+                return redirect(url_for('main.success'))
+
+        # Render the signup page (GET request)
+        return render_template('successpage.html')
+
+    except SQLAlchemyError as e:
+        print(f"An error occurred: {e}")
+
+    return render_template('successpage.html')
+
 @main.route('/login')
 def login():
     return render_template('login.html')
@@ -47,3 +85,7 @@ def login():
 @main.route('/profile')
 def profile():
     return render_template('profile.html')
+
+@main.route('/forget')
+def forget():
+    return render_template('forget.html')
