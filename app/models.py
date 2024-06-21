44795645fd4fbda684edb2581f5f21d3ac6b6482
Ian Tseng <tsengian1@gmail.com>
2024-06-21 17:41:37 -0400

    attempt on models

--- app/models.py
+++ app/models.py
@@ -2,9 +2,13 @@ import random
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+# from nltk.stem import WordNetLemmatizer
+import psycopg2
+from psycopg2 import sql
 
 nltk.download('punkt')
 nltk.download('stopwords')
+# nltk.download('wordnet')
 
 food_recommendations = {
     'vegetarian': ['Vegetable Stir Fry', 'Veggie Burger', 'Pasta Primavera'],
@@ -15,14 +19,14 @@ food_recommendations = {
 }
 
 GREETING_INPUTS = ("hello", "hi", "greetings", "sup", "what's up","hey", "hola", "bonjour", "nihao")
-GREETING_RESPONSES = ["hi", "hey", "*nods*", "hi there", "hello", "I am glad! You are talking to me"]
+GREETING_RESPONSES = ["Hi! How can I help you today?"]
 
 def greeting(sentence):
     """If user's input is a greeting, return a greeting response"""
     for word in sentence:
         if word.lower() in GREETING_INPUTS:
             return random.choice(GREETING_RESPONSES)
-    return None
+    return GREETING_RESPONSES
 # Preprocess input text
 def preprocess_text(text):
     stop_words = set(stopwords.words('english'))
@@ -40,6 +44,57 @@ def generate_response(user_input):
         if category in processed_input:
             return random.choice(food_recommendations[category])
     return "I'm not sure what you would like. Can you specify a dietary preference?"
+
+def extract_data():
+    # Database URL
+    database_url = "postgresql://SavorSense_owner:iX2EMY6TzLlf@ep-shrill-cloud-a40et0x7.us-east-1.aws.neon.tech/SavorSense?sslmode=require"
+
+    # Create a connection to the database
+    conn = psycopg2.connect(database_url)
+
+    # Create a cursor object
+    cur = conn.cursor()
+
+    # SQL query
+    query = sql.SQL("SELECT * FROM {}").format(sql.Identifier('restaurants'))
+
+    # Execute the query
+    cur.execute(query)
+
+    # Fetch all the rows
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    # Close the cursor and connection
+    cur.close()
+    conn.close()
+
+# def preprocess_data():
+#     rows = [row[0] for row in rows]
+
+#     # Initialize the lemmatizer
+#     lemmatizer = WordNetLemmatizer()
+
+#     preprocessed_rows = []
+
+#     for row in rows:
+#         row = str(row)
+#         # Tokenize the text
+#         tokens = word_tokenize(row)
+
+#         # Remove non-alphabetic tokens and convert to lower case
+#         words = [token.lower() for token in tokens if token.isalpha()]
+
+#         # Remove stop words
+#         stop_words = set(stopwords.words('english'))
+#         words = [word for word in words if word not in stop_words]
+
+#         # Lemmatize the words
+#         words = [lemmatizer.lemmatize(word) for word in words]
+
+#         preprocessed_rows.append(words)
 
 # Example usage
 if __name__ == "__main__":
